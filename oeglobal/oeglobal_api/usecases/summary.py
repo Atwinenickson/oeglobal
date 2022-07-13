4365ce4c0d89d22c8d3619b414941502c39abdc5
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-13 17:55:46 +0300

    add details to db

--- oeglobal/oeglobal_api/usecases/summary.py
+++ oeglobal/oeglobal_api/usecases/summary.py
@@ -4,16 +4,19 @@ from article import get_articles
 from posturls import get_urls
 import json
 import sqlite3
-
+from pathlib import Path
+import random
+from multiprocessing import connection
+from datetime import datetime
 
 class OeglobalNewsDetail:
     def __init__(self,url):
         self.url = url
-        self.connection = sqlite3.connect('/mnt/d/Work/Others/Oreg/oeglobal/oeglobal')
+        self.connection = sqlite3.connect(Path(__file__).resolve().parent.parent / 'db.sqlite3')
         self.cursor = self.connection.cursor()
         self.detail={}
 
-    def OeglobalData(self):
+    def OeglobalData(self, url):
         pagelinks = get_urls(url)
         for link in pagelinks:
             dictironaryData = get_articles(link)
@@ -30,10 +33,10 @@ class OeglobalNewsDetail:
     def SaveToDatabase(self,convertedData):
         randomNumber = random.randint(1,1000)
         id = int(datetime.now().microsecond)+randomNumber
-        self.connection.execute('insert into oeglobal_newsdetail values(?,?)',[id,convertedData])
+        self.connection.execute('insert into oeglobal_api_article values(?,?)',[id,convertedData])
         self.connection.commit()
 
 
 
-dictironaryData = OeglobalNewsDetail('https://connect.oeglobal.org/').OeglobalData()
-OeglobalNewsDetail().ConvertToJson(dictironaryData)
+dictironaryData = OeglobalNewsDetail('https://connect.oeglobal.org/').OeglobalData('https://connect.oeglobal.org/')
+OeglobalNewsDetail('https://connect.oeglobal.org/').ConvertToJson(dictironaryData)
