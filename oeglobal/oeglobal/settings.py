7fadf2fdadb78795a3d70f0d3846bd339d646d67
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-15 18:33:00 +0300

    add pagination with limit set to 2

--- oeglobal/oeglobal/settings.py
+++ oeglobal/oeglobal/settings.py
@@ -72,8 +72,8 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': ['rest_framework.pagination.PageNumberPagination'],
-    'PAGE_SIZE': 2
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 WSGI_APPLICATION = 'oeglobal.wsgi.application'
