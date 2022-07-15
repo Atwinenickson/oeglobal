3ebaed98a89f29f917ca754ed91eceedfba9dc40
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-15 17:53:22 +0300

    search, filter and order backends

--- oeglobal/oeglobal/settings.py
+++ oeglobal/oeglobal/settings.py
@@ -70,6 +70,10 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
 WSGI_APPLICATION = 'oeglobal.wsgi.application'
 
 
@@ -125,6 +129,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
-}
--- oeglobal/oeglobal_api/views.py
+++ oeglobal/oeglobal_api/views.py
@@ -8,41 +8,41 @@ from oeglobal_api.models import Article, Topic, TopicURL, Podcast, RecentPodcast
 class ArticleViewSet(viewsets.ModelViewSet):
    queryset = Article.objects.all()
    serializer_class = ArticleSerializer
-   filter_backends = [filters.SearchFilter]
+   filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields = ['ArticleID', 'Title', 'Articleurl', 'Replies', 'Views', 'Date']
    search_fields = ['ArticleID', 'Title', 'Articleurl', 'Replies', 'Views', 'Date']
-   filter_backends = [filters.OrderingFilter]
    ordering_fields = '__all__'
 
 
 class TopicViewSet(viewsets.ModelViewSet):
    queryset = Topic.objects.all()
    serializer_class = TopicSerializer
-   filter_backends = [filters.SearchFilter]
+   filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields = ['TopicID', 'Topic', 'ArticleID']
    search_fields = ['TopicID', 'Topic', 'ArticleID']
-   filter_backends = [filters.OrderingFilter]
    ordering_fields = '__all__'
 
 class TopicURLViewSet(viewsets.ModelViewSet):
    queryset = TopicURL.objects.all()
    serializer_class = TopicURLSerializer
-   filter_backends = [filters.SearchFilter]
+   filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields = ['TopicUrlID', 'TopicUrl', 'TopicID']
    search_fields = ['TopicUrlID', 'TopicUrl', 'TopicID']
-   filter_backends = [filters.OrderingFilter]
    ordering_fields = '__all__'
 
 class PodcastViewSet(viewsets.ModelViewSet):
    queryset = Podcast.objects.all()
    serializer_class = PodcastSerializer
-   filter_backends = [filters.SearchFilter]
+   filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields = ['Title', 'Podcasturl', 'Comments', 'Description', 'Date']
    search_fields = ['Title', 'Podcasturl', 'Comments', 'Description', 'Date']
-   filter_backends = [filters.OrderingFilter]
    ordering_fields = '__all__'
 
 
 class RecentPodcastViewSet(viewsets.ModelViewSet):
    queryset = RecentPodcast.objects.all()
    serializer_class = RecentPodcastSerializer
-   filter_backends = [filters.SearchFilter]
+   filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields =  ['Title', 'RecentPodcasturl', 'Date']
    search_fields = ['Title', 'RecentPodcasturl', 'Date']
-   filter_backends = [filters.OrderingFilter]
    ordering_fields = '__all__'
