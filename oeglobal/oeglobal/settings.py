c5f2d0e0d3d98b8c35ec5236cea683420a6994f2
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-15 18:27:12 +0300

    search, filter and order backends

--- oeglobal/oeglobal/settings.py
+++ oeglobal/oeglobal/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': ['rest_framework.pagination.PageNumberPagination'],
+    'PAGE_SIZE': 2
 }
 
 WSGI_APPLICATION = 'oeglobal.wsgi.application'
